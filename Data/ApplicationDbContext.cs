004e60df3ad2b1a4224e127dc6b6a32420264238
Vitor Gabriel <vitor.life18092004@gmail.com>
2023-07-20 17:19:35 -0300

    Criacao das paginas da propriedade

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,29 @@
-﻿using GeotecnologiaKNS.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using GeotecnologiaKNS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace GeotecnologiaKNS.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
         {
-
         }
+
+        public DbSet<GeotecnologiaKNS.Models.Propriedade> Propriedade { get; set; } = default!;
         public DbSet<Produtor> Produtores { get; set; }
-        public DbSet<Propriedade> Propriedades { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ignorar a classe que não deve ser mapeada para o banco de dados
+            modelBuilder.Ignore<Dictionary<Estado, List<string>>>();
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Data/Migrations/20230720185457_AtualizacaoPropriedade.Designer.cs
+++ Data/Migrations/20230720185457_AtualizacaoPropriedade.Designer.cs
@@ -4,6 +4,7 @@ using GeotecnologiaKNS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeotecnologiaKNS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230720185457_AtualizacaoPropriedade")]
+    partial class AtualizacaoPropriedade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
