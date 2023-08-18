9f318220fca5349f3a5da4c5384aaddb2ac276e7
cleberMargarida <cleberfilho3@gmail.com>
2023-08-18 12:11:38 -0300

    Exibindo lista de solicitacoes em produtores e propriedade

---
+++ Repositories/Interfaces/ISolicitacaoRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace GeotecnologiaKNS.Repositories.Interfaces
+{
+    public interface ISolicitacaoRepository
+    {
+        IEnumerable<Solicitacao> GetSolicitacoesByProdutor(int produtorId);
+        IEnumerable<Solicitacao> GetSolicitacoesByPropriedade(int propriedadeId);
+    }
+}
---
+++ Repositories/SolicitacaoRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace GeotecnologiaKNS.Repositories
+{
+    public class SolicitacaoRepository : ISolicitacaoRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SolicitacaoRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Solicitacao> GetSolicitacoesByPropriedade(int propriedadeId)
+        {
+            return _context.Solicitacao.Include(x => x.Propriedade).Where(x => x.PropriedadeId == propriedadeId);
+        }
+
+        public IEnumerable<Solicitacao> GetSolicitacoesByProdutor(int produtorId)
+        {
+            return _context.Solicitacao.Include(x => x.Propriedade).Where(x => x.Propriedade != null && x.Propriedade.ProdutorId == produtorId);
+        }
+    }
+}
