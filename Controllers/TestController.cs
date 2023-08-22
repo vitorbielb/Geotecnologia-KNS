31c0bb9d88536aac1c50cde6bf09baa8a44f1dbb
cleberMargarida <cleberfilho3@gmail.com>
2023-08-22 10:00:05 -0300

    WIP login page

---
+++ Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeotecnologiaKNS.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
