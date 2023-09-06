9ca46777880e1eff89f4af9b6c3437fbc4d5eee7
cleberMargarida <cleberfilho3@gmail.com>
2023-09-05 21:22:15 -0300

    WIP Users CRUD

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,20 +1,164 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GeotecnologiaKNS.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GeotecnologiaKNS.Controllers
 {
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(
+            ApplicationDbContext context
+            , IPasswordHasher<ApplicationUser> passwordHasher
+            , UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _passwordHasher = passwordHasher;
+            _userManager = userManager;
         }
 
+        // GET: User
         public async Task<IActionResult> Index()
         {
-            var model = await _context.Users.Include(x => x.Industria).ToListAsync();
-            return View(model);
+            return _context.Users != null ?
+                        View(await _context.Users.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [TenantFilter]
+        public async Task<IActionResult> Create(UserViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var user = viewModel.ToModel();
+            _context.Users.Add(user);
+            var hashedPassword = _passwordHasher.HashPassword(user, viewModel.Password);
+            user.SecurityStamp = Guid.NewGuid().ToString();
+            user.PasswordHash = hashedPassword;
+
+            _context.SaveChanges();
+
+            await _userManager.AddToRoleAsync(user, viewModel.Role);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user.ToViewModel());
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UserViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserViewModelExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> GetRolesAsync()
+        {
+            var roles = await _context.Roles.ToListAsync();
+
+            var selectListItems = roles.ToSelectListItems(
+                       x => x.ToString(),
+                       x => x.Id,
+                       options => options.Placeholder = "Selecione...");
+
+            return Ok(selectListItems);
+        }
+
+        private bool UserViewModelExists(string id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ Models/UserViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeotecnologiaKNS.Models
+{
+    public class UserViewModel
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Nome")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Celular")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme senha")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public int TenantId { get; set; }
+
+        [Required]
+        [Display(Name = "Função")]
+        public string Role { get; set; }
+    }
+
+    public static class UserViewModelExtensions
+    {
+        public static ApplicationUser ToModel(this UserViewModel viewModel) => new()
+        {
+            Id = viewModel.Id,
+            PhoneNumber = viewModel.PhoneNumber,
+            UserName = viewModel.UserName,
+            Email = viewModel.Email,
+            TenantId = viewModel.TenantId,
+            NormalizedUserName = viewModel.UserName.Normalize(),
+            NormalizedEmail = viewModel.Email.Normalize(),
+            EmailConfirmed = true,
+        };
+
+        public static UserViewModel ToViewModel(this ApplicationUser model) => new()
+        {
+            Id = model.Id,
+            PhoneNumber = model.PhoneNumber,
+            UserName = model.UserName,
+            Email = model.Email,
+            TenantId = model.TenantId
+        };
+    }
+}
