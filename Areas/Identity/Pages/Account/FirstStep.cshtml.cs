9567d2bf0ffef208eb1f92114e787684a5329cf3
Vitor Gabriel Brito Gonçalves <118298383+vitorbielb@users.noreply.github.com>
2023-09-26 16:45:34 -0300

    remove id firststep

--- Areas/Identity/Pages/Account/FirstStep.cshtml.cs
+++ Areas/Identity/Pages/Account/FirstStep.cshtml.cs
@@ -11,8 +11,6 @@ namespace GeotecnologiaKNS.Areas.Identity.Pages.Account
         {
             _context = context;
         }
-
-        public int Id { get; set; }
         [BindProperty]
         public InputModel Input { get; set; }
 
