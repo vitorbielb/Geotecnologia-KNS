b917e22188e16ab1a3338bfff50812004ed1b5ac
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-04-22 12:00:00 -0300

    Otimização no código Arquivo.cs e Cartografia.cs

--- src/GeotecnologiaKNS/Models/Arquivo.cs
+++ src/GeotecnologiaKNS/Models/Arquivo.cs
@@ -1,64 +1,36 @@
 ﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GeotecnologiaKNS.Models
 {
     public abstract class Arquivo : IPrimaryKeyInfo<int>
     {
-        private const string RequiredMessage = "Campo obrigatório";
-
         public int Id { get; set; }
-
-        [Required(ErrorMessage = RequiredMessage)]
         [DisplayName("Descrição")]
-        [StringLength(200, ErrorMessage = "A descrição deve ter no máximo {1} caracteres.")]
-        public string Descricao { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = RequiredMessage)]
-        public byte[] Dados { get; set; } = Array.Empty<byte>();
-
-        [Required(ErrorMessage = RequiredMessage)]
-        [DisplayName("Tipo de Conteúdo")]
-        [StringLength(150, ErrorMessage = "O ContentType deve ter no máximo {1} caracteres.")]
-        public string ContentType { get; set; } = string.Empty;
-
-        [NotMapped]
-        public abstract int VinculoId { get; set; }
+        public string Descricao { get; set; }
+        public byte[] Dados { get; set; }
+        public string ContentType { get; set; }
+        [NotMapped] public abstract int VinculoId { get; set; }
     }
 
     public class ProdutorArquivo : Arquivo
     {
-        [ForeignKey(nameof(Produtor))]
-        public override int VinculoId { get; set; }
+        [ForeignKey("Produtor")] public override int VinculoId { get; set; }
     }
 
     public class PropriedadeArquivo : Arquivo
     {
-        [ForeignKey(nameof(Propriedade))]
-        public override int VinculoId { get; set; }
+        [ForeignKey("Propriedade")] public override int VinculoId { get; set; }
     }
-
     public class AnaliseArquivo : Arquivo
     {
-        [ForeignKey(nameof(Analise))]
-        public override int VinculoId { get; set; }
-
-        [DisplayName("Data da Análise")]
-        public DateTime DataAnalise { get; set; } = DateTime.Now;
+        [ForeignKey("Analise")] public override int VinculoId { get; set; }
+        public DateTime? DataAnalise { get; set; } = DateTime.Now;
     }
-
     public class CartografiaArquivo : Arquivo
     {
-        [ForeignKey(nameof(Cartografia))]
-        public override int VinculoId { get; set; }
-
-        [DisplayName("Data da Cartografia")]
-        public DateTime DataCartografia { get; set; } = DateTime.Now;
-
-        [Required(ErrorMessage = "Campo obrigatório")]
-        [DisplayName("Tipo")]
-        [StringLength(100, ErrorMessage = "O tipo deve ter no máximo {1} caracteres.")]
-        public string Tipo { get; set; } = string.Empty;
+        [ForeignKey("Cartografia")] public override int VinculoId { get; set; }
+        public DateTime? DataCartografia { get; set; } = DateTime.Now;
+        public string Tipo { get; set; }
     }
 }
--- src/GeotecnologiaKNS/Models/Cartografia.cs
+++ src/GeotecnologiaKNS/Models/Cartografia.cs
@@ -5,20 +5,25 @@ namespace GeotecnologiaKNS.Models
 {
     public class Cartografia : IIndustriaInfo, IPropriedadesInfo, IPrimaryKeyInfo<int>
     {
+        private const string RequiredMessage = "Campo obrigatório";
+
         public int Id { get; set; }
 
         [ForeignKey(nameof(Industria))]
         public int TenantId { get; set; }
-        public Industria? Industria { get; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        public Industria? Industria { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Propriedade")]
         [ForeignKey(nameof(Propriedade))]
         public int PropriedadeId { get; set; }
+
         public Propriedade? Propriedade { get; set; }
+
         public CartografiaArquivo? CartografiaArquivo { get; set; }
 
-        [Display(Name = "Data da solicitação")]
-        public List<CartografiaArquivo>? Arquivos { get; set; }
+        [Display(Name = "Arquivos da Cartografia")]
+        public List<CartografiaArquivo> Arquivos { get; set; } = new();
     }
 }
