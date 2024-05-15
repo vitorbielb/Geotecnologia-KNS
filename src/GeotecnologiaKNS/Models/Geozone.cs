a590938fa48db3470b284dbd04bf81c9f66ff2fe
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-05-15 12:00:00 -0300

    Otimização no código Geozone.cs

--- src/GeotecnologiaKNS/Models/Geozone.cs
+++ src/GeotecnologiaKNS/Models/Geozone.cs
@@ -1,6 +1,6 @@
-﻿using NuGet.Protocol;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 
 namespace GeotecnologiaKNS.Models
 {
@@ -8,16 +8,18 @@ namespace GeotecnologiaKNS.Models
     {
         [Key]
         public int Id { get; set; }
+
         public int TenantId { get; set; }
 
         [NotMapped]
-        public Vertice[] Utm 
-        { 
-            get => UtmAsJson.FromJson<Vertice[]>(); 
-            set => UtmAsJson = value.ToJson(); 
+        public Vertice[] Utm
+        {
+            get => JsonSerializer.Deserialize<Vertice[]>(UtmAsJson) ?? Array.Empty<Vertice>();
+            set => UtmAsJson = JsonSerializer.Serialize(value ?? Array.Empty<Vertice>());
         }
 
         [Column("Utm")]
+        [Required]
         public string UtmAsJson { get; set; } = "[]";
     }
 }
