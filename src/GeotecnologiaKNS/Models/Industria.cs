39faa83067a53dc2801aa144109be695dbcf8830
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-05-23 12:00:00 -0300

    Otimização no código Industria.cs

--- src/GeotecnologiaKNS/Models/Industria.cs
+++ src/GeotecnologiaKNS/Models/Industria.cs
@@ -1,44 +1,47 @@
-﻿using GeotecnologiaKNS.Areas.Identity.Pages.Account;
-using GeotecnologiaKNS.Validators;
+﻿using GeotecnologiaKNS.Validators;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GeotecnologiaKNS.Models
 {
     public class Industria : ITenantInfo
     {
+        private const string RequiredMessage = "Campo obrigatório";
+        private const string NomeLengthMessage = "O nome deve ter no mínimo 2 e no máximo 100 caracteres";
+
         [Key]
         public int TenantId { get; set; }
 
         public byte[]? Imagem { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Nome")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome deve ter no mínimo 2 e no máximo 100 caracteres")]
-        public string Nome { get; set; } = default!;
+        [StringLength(100, MinimumLength = 2, ErrorMessage = NomeLengthMessage)]
+        public string Nome { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Nome resumido")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome deve ter no mínimo 2 e no máximo 100 caracteres")]
-        public string NomeResumido { get; set; } = default!;
+        [StringLength(100, MinimumLength = 2, ErrorMessage = NomeLengthMessage)]
+        public string NomeResumido { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Razão social")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome deve ter no mínimo 2 e no máximo 100 caracteres")]
-        public string RazaoSocial { get; set; } = default!;
+        [StringLength(100, MinimumLength = 2, ErrorMessage = NomeLengthMessage)]
+        public string RazaoSocial { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "CNPJ")]
-        [CnpjValid(ErrorMessage = "Cnpj invalido")]
-        public string Cnpj { get; set; } = default!;
+        [CnpjValid(ErrorMessage = "CNPJ inválido")]
+        [StringLength(18, MinimumLength = 14, ErrorMessage = "CNPJ inválido")]
+        public string Cnpj { get; set; } = string.Empty;
 
-        public ICollection<Produtor>? Produtores { get; set; }
+        public ICollection<Produtor> Produtores { get; set; } = new List<Produtor>();
 
-        public ICollection<Propriedade>? Propriedades { get; set; }
+        public ICollection<Propriedade> Propriedades { get; set; } = new List<Propriedade>();
 
-        public ICollection<Solicitacao>? Solicitacoes { get; set; }
-        public ICollection<Cartografia>? Cartografias { get; set; }
+        public ICollection<Solicitacao> Solicitacoes { get; set; } = new List<Solicitacao>();
 
-        public ICollection<ApplicationUser>? Usuarios { get; set; }
+        public ICollection<Cartografia> Cartografias { get; set; } = new List<Cartografia>();
+
+        public ICollection<ApplicationUser> Usuarios { get; set; } = new List<ApplicationUser>();
     }
 }
