cc63d51e048dd80f6478881c3542141721cdf88c
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-05-28 12:00:00 -0300

    Otimização no código Produtor.cs/3

--- src/GeotecnologiaKNS/Models/Produtor.cs
+++ src/GeotecnologiaKNS/Models/Produtor.cs
@@ -5,27 +5,30 @@ namespace GeotecnologiaKNS.Models
 {
     public class Produtor : IIndustriaInfo, IPrimaryKeyInfo<int>
     {
+        private const string RequiredMessage = "Campo obrigatório";
+
         [Key]
         public int Id { get; set; }
 
         [ForeignKey(nameof(Industria))]
         public int TenantId { get; set; }
-        public Industria? Industria { get; } = default!;
 
-        [Required(ErrorMessage = "Campo obrigatório")]
-        [Display(Name = "Produtor")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "O nome deve ter no mínimo 6 e no máximo 100 caracteres")]
-        public string Nome { get; set; } = default!;
+        public Industria? Industria { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Produtor Rural")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "O nome deve ter no mínimo 4 e no máximo 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "CPF/CNPJ")]
-        public string Cpf { get; set; } = default!;
+        [StringLength(18, MinimumLength = 11, ErrorMessage = "O CPF/CNPJ deve ter entre 11 e 18 caracteres")]
+        public string Cpf { get; set; } = string.Empty;
 
-        public List<ProdutorArquivo>? Documentos { get; set; }
+        public List<ProdutorArquivo> Documentos { get; set; } = new();
 
-        public List<Propriedade>? Propriedades { get; set; }
+        public List<Propriedade> Propriedades { get; set; } = new();
 
-        public Solicitacao? Solicitacoes { get; set; }
+        public List<Solicitacao> Solicitacoes { get; set; } = new();
     }
-
 }
