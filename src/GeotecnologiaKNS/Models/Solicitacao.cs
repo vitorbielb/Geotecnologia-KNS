cb89ff18bfc5dcb04e36cb3c5fa2480c37a5afd6
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-07-03 12:00:00 -0300

    Alteração na estrutura do código Solicitacao.cs

--- src/GeotecnologiaKNS/Models/Solicitacao.cs
+++ src/GeotecnologiaKNS/Models/Solicitacao.cs
@@ -6,25 +6,29 @@ namespace GeotecnologiaKNS.Models
     public class Solicitacao : IIndustriaInfo, IPropriedadeInfo, ICartografiasInfo, IPrimaryKeyInfo<int>
     {
         private const string RequiredMessage = "Campo obrigatório";
+        private const string TextoLongoMessage = "O campo {0} deve ter no máximo {1} caracteres.";
 
         public int Id { get; set; }
 
         [ForeignKey(nameof(Industria))]
         public int TenantId { get; set; }
 
-        public Industria? Industria { get; }
+        public Industria? Industria { get; set; }
 
         [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Imóvel Rural")]
+        [ForeignKey(nameof(Propriedade))]
         public int PropriedadeId { get; set; }
 
         public Propriedade? Propriedade { get; set; }
 
         [Display(Name = "Responsável pela Análise")]
+        [StringLength(150, ErrorMessage = TextoLongoMessage)]
         public string? Analista { get; set; }
 
         [Required(ErrorMessage = RequiredMessage)]
         [Display(Name = "Responsável pela Solicitação")]
+        [StringLength(150, ErrorMessage = TextoLongoMessage)]
         public string Solicitante { get; set; } = string.Empty;
 
         [Display(Name = "Data de Abertura")]
@@ -36,18 +40,20 @@ namespace GeotecnologiaKNS.Models
         public Cartografia? Cartografia { get; set; }
 
         [Display(Name = "Observações")]
+        [StringLength(2000, ErrorMessage = TextoLongoMessage)]
         public string? Observacao { get; set; }
 
         [Display(Name = "Resultado da Análise")]
+        [StringLength(2000, ErrorMessage = TextoLongoMessage)]
         public string? Parecer { get; set; }
 
         public List<AnaliseArquivo> Documentos { get; set; } = new();
 
         [Display(Name = "Situação da Solicitação")]
-        public Status Status { get; set; } = Status.Solicitado;
+        public StatusSolicitacao Status { get; set; } = StatusSolicitacao.Solicitado;
     }
 
-    public enum Status
+    public enum StatusSolicitacao
     {
         Solicitado = 0,
         Liberado = 1,
