2529608fb758ab833f75c85d5a6701a518a711be
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-09-18 12:00:00 -0300

    Alteração na estrutura do código ViewHelperValidacao.cs

--- src/GeotecnologiaKNS/Utils/ViewHelpersValidacao.cs
+++ src/GeotecnologiaKNS/Utils/ViewHelpersValidacao.cs
@@ -1,71 +1,49 @@
 ﻿using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace GeotecnologiaKNS.Views
 {
     public static class ViewHelperValidacao
     {
-        public static IHtmlContent DisplayValidacao<TModel>(this IHtmlHelper<TModel> htmlHelper, Validacao validacao)
+        private static readonly IReadOnlyDictionary<Validacao, string> ValidacaoCssClassMap =
+            new Dictionary<Validacao, string>
+            {
+                [Validacao.Validado] = "success",
+                [Validacao.Pendente] = "secondary"
+            };
+
+        public static IHtmlContent DisplayValidacao<TModel>(
+            this IHtmlHelper<TModel> htmlHelper,
+            Validacao validacao)
         {
-            return new HtmlString($"<a class=\"btn btn-{_validacaoDict[validacao]}\">{validacao}</a>");
+            var cssClass = GetCssClass(validacao);
+            return new HtmlString($"<a class=\"btn btn-{cssClass} status-btn\">{validacao}</a>");
         }
 
-
-        public static IHtmlContent DisplayValidacaoFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        public static IHtmlContent DisplayValidacaoFor<TModel>(
+            this IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, Validacao>> expression)
         {
-            ValidacaoGetter validacaoGetter = new ValidacaoGetter();
-            validacaoGetter.Visit(expression);
-            var validacao = validacaoGetter.Value ?? throw new ArgumentNullException();
-            return new HtmlString($"<a class=\"btn btn-{_validacaoDict[validacao]}  status-btn\"> {validacao}</a>");
+            ArgumentNullException.ThrowIfNull(htmlHelper);
+            ArgumentNullException.ThrowIfNull(expression);
+
+            var model = htmlHelper.ViewData.Model;
+
+            if (model is null)
+                return HtmlString.Empty;
+
+            var validacao = expression.Compile().Invoke(model);
+            var cssClass = GetCssClass(validacao);
+
+            return new HtmlString($"<a class=\"btn btn-{cssClass} status-btn\">{validacao}</a>");
         }
 
-
-        static Dictionary<Validacao, string> _validacaoDict = new()
+        private static string GetCssClass(Validacao validacao)
         {
-            { Validacao.Validado, "success" },
-            { Validacao.Pendente, "secondary" }
-        };
-
-        class ValidacaoGetter : ExpressionVisitor
-        {
-            private Func<object?, object?>? get;
-
-            public Validacao? Value { get; set; }
-
-            protected override Expression VisitMember(MemberExpression node)
-            {
-                if (node.Type.Equals(typeof(Validacao)) && node.Member is PropertyInfo prop)
-                {
-                    this.get = prop.GetValue;
-                }
-
-                return base.VisitMember(node);
-            }
-
-            protected override Expression VisitConstant(ConstantExpression node)
-            {
-                var fieldInfos = node
-                    .Value?
-                    .GetType()
-                    .GetFields();
-
-                if (fieldInfos?.FirstOrDefault() is FieldInfo field &&
-
-                    field.FieldType
-                         .GetProperties()
-                         .Select(x => x.PropertyType)
-                         .Contains(typeof(Validacao))
-                         &&
-
-                    (get?.Invoke(fieldInfos[0].GetValue(node.Value)) is Validacao value))
-                {
-                    Value = value;
-                };
-
-                return base.VisitConstant(node);
-            }
+            return ValidacaoCssClassMap.TryGetValue(validacao, out var cssClass)
+                ? cssClass
+                : "secondary";
         }
     }
 }
