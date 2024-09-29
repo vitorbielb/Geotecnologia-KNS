dad8cad47de5823416f344946e251e92418a3641
Vitor Gabriel Brito Gonçalves <vitor.life18092004@gmail.com>
2024-09-29 12:00:00 -0300

    Alteração na estrutura do código ProdutorArquivoViewModelValidator/4.cs

--- src/GeotecnologiaKNS/Validators/ProdutorArquivoViewModelValidator.cs
+++ src/GeotecnologiaKNS/Validators/ProdutorArquivoViewModelValidator.cs
@@ -1,33 +1,39 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeotecnologiaKNS.Validators
 {
     public class ProdutorArquivoViewModelValidator : AbstractValidator<ProdutorArquivoViewModel>
     {
         private const int LimiteDeArquivos = 5;
-        private readonly ApplicationDbContext context;
+        private readonly ApplicationDbContext _context;
 
         public ProdutorArquivoViewModelValidator(ApplicationDbContext context)
         {
-            this.context = context;
+            _context = context;
 
-            RuleFor(arquivo => arquivo)
+            RuleFor(x => x)
                 .Must(MenosArquivosQueLimite)
                 .WithMessage("Limite de arquivos atingido.");
 
-            RuleFor(x => x.Descricao).NotEmpty();
-            RuleFor(x => x.Dados).NotEmpty();
+            RuleFor(x => x.Descricao)
+                .NotEmpty()
+                .WithMessage("Descrição é obrigatória.");
+
+            RuleFor(x => x.Dados)
+                .NotEmpty()
+                .WithMessage("Arquivo é obrigatório.");
         }
 
-        private bool MenosArquivosQueLimite(ProdutorArquivoViewModel arg)
+        private bool MenosArquivosQueLimite(ProdutorArquivoViewModel model)
         {
-            var documentos = context.Produtores
-                                    .Include(x => x.Documentos)
-                                    .First(x => x.Id == arg.VinculoId)
-                                    .Documentos!
-                                    .ToList();
+            var quantidade = _context.Produtores
+                .AsNoTracking()
+                .Where(x => x.Id == model.VinculoId)
+                .Select(x => x.Documentos!.Count)
+                .FirstOrDefault();
 
-            return documentos.Count() <= LimiteDeArquivos;
+            return quantidade < LimiteDeArquivos;
         }
     }
 }
